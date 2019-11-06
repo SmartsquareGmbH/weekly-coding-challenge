1c45becee4bc9b467d01d0982d2e54bdf4812172
Dennis Dierkes <dierkes@smartsquare.de>
2019-11-06 13:55:00 +0100

    Add locust test

---
+++ prime-factors-kata/load-testing/locustfile.py
@@ -0,0 +1,14 @@
+from locust import HttpLocust, TaskSet, task
+import random
+import os
+
+class StressTest(TaskSet):
+    def on_start(self):
+        self.n = str(random.choice([4211, 4513, 5323, 5807, 6247, 6247, 9923]))
+
+    @task
+    def fetch_prime_faktor(self):
+        self.client.get("/generate/" + self.n)
+
+class Instructor(HttpLocust):
+    task_set = StressTest
