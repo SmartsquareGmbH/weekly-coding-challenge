5b9b72b41ba2f07c598654a349e9b389394ce5ed
Jens Krafczyk <21033189+jkrafczyk-sq@users.noreply.github.com>
2019-11-06 15:45:59 +0100

    Added more example requests to locust; Added result verification to locust.

--- prime-factors-kata/testing/locustfile.py
+++ prime-factors-kata/testing/locustfile.py
@@ -1,14 +1,30 @@
 from locust import HttpLocust, TaskSet, task
 import random
 import os
+import json
+
+EXPECTED_OUTPUTS = {
+    256: [2, 2, 2, 2, 2, 2, 2, 2],
+    4294967295: [3, 5, 17, 257, 65537],
+    2147483649: [3, 715827883],
+    4211: [4211],
+    4513: [4513],
+    5323: [5323],
+    5807: [5807],
+    6247: [6247],
+    9923: [9923],
+}
 
 class StressTest(TaskSet):
     def on_start(self):
-        self.n = str(random.choice([4211, 4513, 5323, 5807, 6247, 6247, 9923]))
+        self.n = random.choice(EXPECTED_OUTPUTS.keys())
+        self.expected_response = EXPECTED_OUTPUTS[self.n]
 
     @task
     def fetch_random_prime_factor(self):
-        self.client.get("/generate/" + self.n)
+        response = self.client.get("/generate/%i" % self.n)
+        assert response.status_code is 200, "Unexpected response code: %i" % response.status_code
+        assert json.loads(response.text) == self.expected_response
 
 class Instructor(HttpLocust):
     task_set = StressTest
