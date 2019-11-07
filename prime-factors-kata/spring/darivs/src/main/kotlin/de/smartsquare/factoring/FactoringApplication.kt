7674fef172b1bd47ee5bc2315dea2442639a8458
darivs <dtack192@gmail.com>
2019-11-07 10:29:11 +0100

    Initialize

---
+++ prime-factors-kata/spring/darivs/src/main/kotlin/de/smartsquare/factoring/FactoringApplication.kt
@@ -0,0 +1,11 @@
+package de.smartsquare.factoring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FactoringApplication
+
+fun main(args: Array<String>) {
+	runApplication<FactoringApplication>(*args)
+}
---
+++ prime-factors-kata/spring/darivs/src/test/kotlin/de/smartsquare/factoring/FactoringApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.smartsquare.factoring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FactoringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
