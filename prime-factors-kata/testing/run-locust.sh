50eff21e9f51a4a8de731c1d9192662b2174378b
Jens Krafczyk <21033189+jkrafczyk-sq@users.noreply.github.com>
2019-11-07 18:02:01 +0100

    Load test script doing multiple separate runs with different thread counts; Add jupyter notebook with some plots.

--- prime-factors-kata/testing/run-locust.sh
+++ prime-factors-kata/testing/run-locust.sh
@@ -28,4 +28,23 @@ else
 fi
 
 docker build . -t locust-coding-challenge
-docker run ${DOCKER_ARGS[@]} -v "$(pwd)/csv:/csv" --rm --name locust-coding-challenge locust-coding-challenge "--host=$TARGET_HOST" --csv=/csv/$NAME
+run_locust() {
+    local run=$1
+    shift
+    mkdir -p csv/$NAME/$run logs/$NAME
+    docker run ${DOCKER_ARGS[@]} \
+        -v "$(pwd)/csv:/csv" \
+        --rm \
+        locust-coding-challenge \
+        "--host=$TARGET_HOST" \
+        --csv=/csv/$NAME/$run/locust \
+        $@ 2>&1 | tee logs/$NAME/$run.log
+}
+
+run_locust 0050 -c=50  -r=5  -t=45s
+run_locust 0100 -c=100 -r=10 -t=45s
+run_locust 0200 -c=200 -r=20 -t=45s
+run_locust 0300 -c=300 -r=30 -t=45s
+run_locust 0500 -c=500 -r=50 -t=45s
+run_locust 0750 -c=750 -r=75 -t=45s
+run_locust 1000 -c=1000 -r=50 -t=45s
