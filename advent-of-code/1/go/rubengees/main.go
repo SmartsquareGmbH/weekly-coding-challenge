0e0383f0ea1c2a88522a166306ccf688c8aea234
Ruben Gees <rubengees7@googlemail.com>
2019-12-01 22:31:31 +0100

    Add go solution for day 1 part 2

--- advent-of-code/1/go/rubengees/main.go
+++ advent-of-code/1/go/rubengees/main.go
@@ -10,13 +10,9 @@ import (
 
 func main() {
 	numbers := readNumbers("input.txt")
-	calculatedSum := 0
 
-	for _, number := range numbers {
-		calculatedSum += calculate(number)
-	}
-
-	fmt.Printf("%d\n", calculatedSum)
+	part1(numbers)
+	part2(numbers)
 }
 
 // Read all numbers from the given filePath and returns them as an array of float64s.
@@ -52,10 +48,42 @@ func readNumbers(filePath string) []float64 {
 	return result
 }
 
+func part1(numbers []float64) {
+	calculatedSum := 0
+
+	for _, number := range numbers {
+		calculatedSum += calculate(number)
+	}
+
+	fmt.Printf("%d\n", calculatedSum)
+}
+
+func part2(numbers []float64) {
+	calculatedSum := 0
+
+	for _, number := range numbers {
+		calculatedSum += calculateRecursive(number)
+	}
+
+	fmt.Printf("%d\n", calculatedSum)
+}
+
 // Calculate the fuel needed by the given module and returns the amount as an int.
 func calculate(module float64) int {
 	divisionResult := module / 3.0
 	rounded := int(divisionResult)
 
 	return rounded - 2
+}
+
+// Calculate the fuel needed by the given module recursively and returns the amount as an int.
+// Recursively means, that for each fuel, the fuel is taken as the next module and all results are summed.
+func calculateRecursive(module float64) int {
+	result := calculate(module)
+
+	if result >= 1 {
+		return result + calculateRecursive(float64(result))
+	} else {
+		return 0
+	}
 }
--- advent-of-code/1/go/rubengees/main_test.go
+++ advent-of-code/1/go/rubengees/main_test.go
@@ -26,3 +26,24 @@ func TestCalculate(t *testing.T) {
 		})
 	}
 }
+
+var dataRecursive = []struct {
+	in  float64
+	out int
+}{
+	{14.0, 2},
+	{1969.0, 966},
+	{100756.0, 50346},
+}
+
+func TestCalculateRecursive(t *testing.T) {
+	for _, row := range dataRecursive {
+		t.Run(fmt.Sprintf("%g", row.in), func(t *testing.T) {
+			result := calculateRecursive(row.in)
+
+			if result != row.out {
+				t.Errorf("Expected %d, but got %d", row.out, result)
+			}
+		})
+	}
+}
