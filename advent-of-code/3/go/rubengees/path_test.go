ba88970e21e16465af082f6fc2e7f71e4bb32dfb
Ruben Gees <rubengees7@googlemail.com>
2019-12-03 21:49:50 +0100

    Fix tests

--- advent-of-code/3/go/rubengees/path_test.go
+++ advent-of-code/3/go/rubengees/path_test.go
@@ -22,7 +22,7 @@ func Test_findClosestIntersection(t *testing.T) {
 				path1: parse("R8,U5,L5,D3"),
 				path2: parse("U7,R6,D4,L4"),
 			},
-			wantIntersection: Intersection{Point: &Point{x: 3, y: 3}, manhattanDistance: 6},
+			wantIntersection: Intersection{x: 3, y: 3, manhattanDistance: 6},
 			wantErr:          false,
 		},
 		{
@@ -31,7 +31,7 @@ func Test_findClosestIntersection(t *testing.T) {
 				path1: parse("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
 				path2: parse("U62,R66,U55,R34,D71,R55,D58,R83"),
 			},
-			wantIntersection: Intersection{Point: &Point{x: 155, y: 4}, manhattanDistance: 159},
+			wantIntersection: Intersection{x: 155, y: 4, manhattanDistance: 159},
 			wantErr:          false,
 		},
 		{
@@ -40,7 +40,7 @@ func Test_findClosestIntersection(t *testing.T) {
 				path1: parse("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
 				path2: parse("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"),
 			},
-			wantIntersection: Intersection{Point: &Point{x: 124, y: 11}, manhattanDistance: 135},
+			wantIntersection: Intersection{x: 124, y: 11, manhattanDistance: 135},
 			wantErr:          false,
 		},
 	}
@@ -74,7 +74,7 @@ func Test_findIntersections(t *testing.T) {
 				path1: Path{PathPart{direction: UP, amount: 3}, PathPart{direction: RIGHT, amount: 4}},
 				path2: Path{PathPart{direction: RIGHT, amount: 4}, PathPart{direction: UP, amount: 3}},
 			},
-			want: []Intersection{{Point: &Point{x: 4, y: 3}, manhattanDistance: 7}},
+			want: []Intersection{{x: 4, y: 3, manhattanDistance: 7}},
 		},
 	}
 	for _, tt := range tests {
